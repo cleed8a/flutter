00106693f6ff9130466b3e909b59ca23c1730ee7
Chang J Lee <lcj861@gmail.com>
2023-03-26 15:06:05 -0500

    contact app practice completed

--- module2/lib/main.dart
+++ module2/lib/main.dart
@@ -11,45 +11,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Layout-Challenge-Specs",
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text("Layout-Challege-Specs"),
-          backgroundColor: Colors.teal,
-        ),
-        body: Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 100,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    width: 100,
-                    height: 100,
+        title: "My Business Card",
+        home: Scaffold(
+            backgroundColor: Colors.black,
+            body: SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/profile_pic.jpg'),
+                  radius: 100,
+                ),
+                Text("James Parker",
+                    style: TextStyle(
+                      fontFamily: 'Alkatra',
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white,
+                      letterSpacing: 3,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text("DEVELOPER",
+                      style: TextStyle(
+                        fontFamily: 'Alkatra',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        letterSpacing: 1.5,
+                      )),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                    height: 10.0,
+                    color: Colors.white,
                   ),
-                  Container(
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                  )
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                ),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: ListTile(
+                        leading: Icon(Icons.phone,
+                        color: Colors.teal,),
+                        title: Text(
+                          "224-206-2000",
+                          style: TextStyle(
+                            fontFamily: 'Alkatra',
+                            fontSize: 16,
+                            color: Colors.black,
+                            letterSpacing: 1.5,
+                          ),
+                        ),
+                      ),
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: ListTile(
+                        leading: Icon(Icons.email,
+                        color: Colors.teal,),
+                        title: Text("testing@email.com",
+                            style: TextStyle(
+                                fontFamily: 'Alkatra',
+                                fontSize: 16,
+                                color: Colors.black,
+                                letterSpacing: 1.5)),
+                      ),
+                    ))
+              ],
+            ))));
   }
 }
