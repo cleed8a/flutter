8ef07a04620a6792db315e8b94dce683d108e8b0
CJ <changlee.d8a@gmail.com>
2023-03-26 22:23:03 -0500

    dice project completed in module3

--- module3/lib/main.dart
+++ module3/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,10 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int dice1num = 1;
+  int dice2num = 1;
+
+  void newnum(){
+    setState(() {
+      dice1num = Random().nextInt(6) + 1;
+      dice2num = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child:
+                TextButton(
+                    onPressed: (){
+                      newnum();
+                    },
+                    child: Image.asset('images/dice$dice1num.png'))),
+          Expanded(
+              child:
+                TextButton(
+                    onPressed: (){
+                      newnum();
+                    },
+                    child: Image.asset('images/dice$dice2num.png'))),
+        ],
+      ),
+    );
   }
 }
 
